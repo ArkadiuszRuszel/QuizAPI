bd52f28b96ab76ad28fa4b53c3f7230179d09eb9
Arkadiusz Ruszel <a.ruszel@hotmail.com>
2021-02-11 10:35:39 +0100

    Quizowanie

---
+++ QuizAPI/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizAPI.Startup))]
+namespace QuizAPI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
